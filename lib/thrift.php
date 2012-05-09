c1a805563a732b5bbb9ca22c66f351cd48e9cb90
Alfred Rossi <alfredr@gmail.com>
2012-05-09 17:27:24 -0400

    add failover scheduling for vaedb

--- lib/thrift.php
+++ lib/thrift.php
@@ -15,8 +15,24 @@ function _vae_dbd($port = 9091) {
   return $_VAE['vaedbd'];
 };
 
+function _vae_vaedb_backends() {
+  global $_VAE;
+
+  $vaedb_backends = array();
+  foreach($_VAE['vaedbd_backend_tiers'] as $tier) {
+    shuffle($tier);
+    $vaedb_backends = array_merge($vaedb_backends, $tier);
+  }
+
+  return $vaedb_backends;
+};
+
 function _vae_thrift_open($client_class, $port) {
   global $_VAE;
+
+  $GLOBALS['CONFIG_ROOT'] = '/www/vae_remote/shared/config';
+  require_once $GLOBALS['CONFIG_ROOT'].'/config.php';
+
   $GLOBALS['THRIFT_ROOT'] = '/www/vae_thrift/current/php/vendor/thrift';
   require_once $GLOBALS['THRIFT_ROOT'].'/Thrift.php';
   require_once $GLOBALS['THRIFT_ROOT'].'/protocol/TBinaryProtocol.php';
@@ -25,13 +41,18 @@ function _vae_thrift_open($client_class, $port) {
   require_once $GLOBALS['THRIFT_ROOT'].'/../../../gen-php/vae/VaeDb.php';
   require_once $GLOBALS['THRIFT_ROOT'].'/../../../gen-php/vae/VaeRubyd.php';
   require_once $GLOBALS['THRIFT_ROOT'].'/../../../gen-php/vae/vae_types.php';
+
+  if(!($backends = _vae_vaedb_backends())) {
+    throw new VaeException("", "No VaeDb backends configured");
+  }
+  
   $i = 0;
   while ($i < 4) {
+    $_VAE['thrift_host'] = array_shift($backends);
+
     try {
-      $_VAE['thrift_host'] = 'localhost';
-      if (!_vae_prod() && (($i % 2) == 0)) $_VAE['thrift_host'] = 'vaedb0.***REMOVED***';
       $socket = new TSocket($_VAE['thrift_host'], $port);
-      $socket->setRecvTimeout(10000);
+      $socket->setRecvTimeout(30000);
       $transport = new TBufferedTransport($socket, 1024, 1024);
       $protocol = new TBinaryProtocol($transport);
       $client = new $client_class($protocol);
@@ -39,8 +60,11 @@ function _vae_thrift_open($client_class, $port) {
       return $client;
     } catch (TException $tx) {
     }
+
+    if(!$backends) { $backends = _vae_vaedb_backends(); }
     sleep(1);
     $i++;
   }
+
   throw new VaeException("", $tx->getMessage());
 }
