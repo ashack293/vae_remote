88d16170f039f08ed5b8c0b05f25b57c64903fd2
root <root@m.a0.c0.hosting.cloud.actionverb.com>
2012-08-27 14:41:46 -0400

    manually force feed requests to vae0

--- lib/rest.php
+++ lib/rest.php
@@ -170,7 +170,12 @@ function _vae_simple_rest($url, $post_data = null, $header = false) {
     return array_shift($_VAE['mock_rest']);
   }
   if (!strstr($url, "://")) {
-    $url = $_VAE['config']['backlot_url'] . $url;
+    if (substr($url, 0, 5) == "/feed") {
+      $url = "http://vae0.***REMOVED***" . $url;
+      $header = $_VAE['config']['backlot_url'];
+    } else {
+      $url = $_VAE['config']['backlot_url'] . $url;
+    }
   }
   $ch = curl_init($url);
   curl_setopt($ch, CURLOPT_TIMEOUT, 600);
