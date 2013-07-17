626ad7a49ddbe1d5b92ccaaa14ce5eab3a90049f
Kevin Bombino <kevin@bombino.org>
2013-07-17 10:33:17 -0700

    dont need fix doc root

--- lib/pages.php
+++ lib/pages.php
@@ -2,9 +2,6 @@
 
 function _vae_page() {
   global $_VAE;
-  if (!strlen($_REQUEST['__page'])) {
-    _vae_page_fix_document_root();
-  }
   $a = explode(".", $_REQUEST['__page']);
   _vae_page_find($a[0]);
   _vae_page_check_redirects();
@@ -110,14 +107,6 @@ function _vae_page_find($page) {
     memcache_set($_VAE['memcached'], $_VAE['global_cache_key'] . "path2" . $page, array());
   }
   return false;
-}
-
-function _vae_page_fix_document_root() {
-  $root = $_SERVER['DOCUMENT_ROOT'];
-  foreach (array(".html", ".haml", ".php") as $ext) {
-    if (file_exists($root . "/index" . $ext)) return;
-  }
-  file_put_contents($root . "/index.html", "<html><body></body></html>");
 }
 
 function _vae_page_locale_callback($matches) {
