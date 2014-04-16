c77f2b7b678142a72765d848ac5efea218a5c8af
Alfred Rossi <alfredr@gmail.com>
2014-04-16 18:36:19 -0400

    add weight method to preset_weights

--- lib/oscommerce/preset_rates.php
+++ lib/oscommerce/preset_rates.php
@@ -2,13 +2,16 @@
 
 class preset_rates {
   function quote($method = '') {
-    global $method, $shipping_num_items, $shipping_subtotal, $order;
+    global $method, $shipping_num_items, $shipping_subtotal, $shipping_weight, $order;
     $cost = 0;
     if (is_numeric($method['per_order'])) {
       $cost += $method['per_order'];
     }
     if (is_numeric($method['per_item'])) {
       $cost += $shipping_num_items * $method['per_item'];
+    }
+    if (is_numeric($method['per_lb'])) {
+      $cost += $shipping_weight * $method['per_lb'];
     }
     if (is_numeric($method['percentage'])) {
       $cost += $shipping_subtotal * ($method['percentage'] / 100);
