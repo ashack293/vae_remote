40a355b4a219439fc3a104a7d11b3ba973420c64
jlarmstrong <jrod@jaredarmstrong.name>
2014-10-15 14:20:15 -0400

    Temporarily disable check again

--- lib/haml.php
+++ lib/haml.php
@@ -49,9 +49,10 @@ function _vae_sass_deps($sass, $include_directory) {
         $inc_dir = (substr($filename, 0, 1) == "/" ? "" : $include_directory . "/");
         if (!strstr($filename, ".") && !stristr($filename,'vendor/') && !stristr($filename,'vendors/') && !stristr($filename,'compass')) {
           $tmp_filename = (strrchr($filename,"/") == false) ? "_". $filename : substr($filename, 0, strpos($filename,strrchr($filename,"/")) + 1 ) . "_" . substr(strrchr($filename,"/"),1);
-          if (file_exists($inc_dir . $tmp_filename . ".scss")) {
+          /*if (file_exists($inc_dir . $tmp_filename . ".scss")) {
             $filename = $tmp_filename . ".scss";
-          }elseif (file_exists($inc_dir . $filename . ".scss")) {
+          }else*/
+          if (file_exists($inc_dir . $filename . ".scss")) {
             $filename = $filename . ".scss";
           } else {
             $filename = $filename . ".sass";
