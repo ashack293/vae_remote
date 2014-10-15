e528fc7a84090c7c17d714b9caef34e1a49a7c80
jlarmstrong <jrod@jaredarmstrong.name>
2014-10-15 14:17:00 -0400

    exclude compass

--- lib/haml.php
+++ lib/haml.php
@@ -47,7 +47,7 @@ function _vae_sass_deps($sass, $include_directory) {
       $filename = str_replace(array("'", '"',';'), "", $match[1]);
       if (!strstr($filename, ".") || strstr($filename, ".sass") || strstr($filename, ".scss")) {
         $inc_dir = (substr($filename, 0, 1) == "/" ? "" : $include_directory . "/");
-        if (!strstr($filename, ".") && !stristr($filename,'vendor/') && !stristr($filename,'vendors/')) {
+        if (!strstr($filename, ".") && !stristr($filename,'vendor/') && !stristr($filename,'vendors/') && !stristr($filename,'compass')) {
           $tmp_filename = (strrchr($filename,"/") == false) ? "_". $filename : substr($filename, 0, strpos($filename,strrchr($filename,"/")) + 1 ) . "_" . substr(strrchr($filename,"/"),1);
           if (file_exists($inc_dir . $tmp_filename . ".scss")) {
             $filename = $tmp_filename . ".scss";
