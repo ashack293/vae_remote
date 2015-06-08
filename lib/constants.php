901d342bec900e595de638c101d8e6c4bdc9ef0f
jlarmstrong <jrod@jaredarmstrong.name>
2015-06-08 14:46:22 -0400

    Add item prop to v:a allowed attributes

--- lib/constants.php
+++ lib/constants.php
@@ -383,7 +383,7 @@ $_VAE['us48'] = array(
 );
 
 $_VAE['attributes'] = array(
-  'a' => array('charset','coords','href','hreflang','name','rel','rev','shape','target','type'),
+  'a' => array('charset','coords','href','hreflang','itemprop','name','rel','rev','shape','target','type'),
   'collection' => array("all","default_page","filter_input","groups","id","max_pages","nested","next","order","output_order","page_select","paginate","path","per_row","previous","skip","store_in_session","wrap"),
   'form' => array('accept','action','accept-charset','ajax','enctype','flash','method','name','target','validateinline'),
   'img' => array('align','alt','border','height','hspace','ismap','longdesc','placeholder','src','usemap','vspace','width'),
