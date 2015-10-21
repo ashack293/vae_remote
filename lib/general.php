1bef34369bdd599c4657b88de423599d4a2babdc
Kevin Bombino <kevin@bombino.org>
2015-10-20 21:12:31 -0700

    add total line to Vae timer

--- lib/general.php
+++ lib/general.php
@@ -1536,6 +1536,7 @@ function _vae_render_timer() {
   foreach ($_VAE['ticks'] as $r) {
     $ticks .= "<tr><td>" . $r[0] . "</td><td align='right'>" . number_format($r[1]*100/$sum, 3) . "%</td><td align='right'>" . number_format($r[1], 3) . "ms</td></tr>\n";
   }
+  $ticks .= "<tr><td>Total</td><td></td><td align='right'>" . number_format($sum, 3) . "ms</td></tr>\n";
   return _vae_render_message("Vae Timer on ".gethostname(), "<h2>Vae Timer</h2><div class='b'><table style='width: 100%;'>" . $ticks . "</table></div>");
 }
 
