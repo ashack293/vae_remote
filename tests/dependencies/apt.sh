ce1163832ea835550ae5201c822a7af1f1a8fd92
Kevin Bombino <kevin@bombino.org>
2016-04-06 18:48:47 -0700

    Add missing file

---
+++ tests/dependencies/apt.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+pkgs=(libantlr3c-dev libzmq3-dev libboost-filesystem-dev libboost-program-options-dev libboost-system-dev libboost-thread-dev libpcre3-dev libmemcached-dev libmysqlcppconn-dev)
+
+# Work from the directory CI will cache
+mkdir -p ~/apt-cache
+cd ~/apt-cache
+
+# check we have a deb for each package
+useCache=true
+for pkg in "${pkgs[@]}"; do
+    if ! ls | grep "^${pkg}"; then
+        useCache=false
+    fi
+done
+
+set -x
+
+if [ ${useCache} == true ]; then
+    sudo dpkg -i *.deb
+    exit 0
+fi
+
+sudo add-apt-repository -y ppa:chris-lea/zeromq
+sudo apt-get update
+sudo apt-get install --reinstall "${pkgs[@]}"
+
+cp -v /var/cache/apt/archives/*.deb ~/apt-cache
