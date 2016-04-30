4c39a880eabb86aaf0ade9e3ae4c19b58ec9c699
Kevin Bombino <kevin@bombino.org>
2016-04-29 19:00:39 -0700

    Dont report local errors to logstash

--- lib/general.php
+++ lib/general.php
@@ -887,7 +887,8 @@ function _vae_log($msg) {
 }
 
 function _vae_logstash_send($log_msg) {
-  if ($_ENV['TEST']) return;
+  global $_VAE;
+  if ($_ENV['TEST'] || $_VAE['local_full_stack']) return;
   $sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
   @socket_sendto($sock, $log_msg, strlen($log_msg), 0, "public.logs***REMOVED***", 5000);
 }
