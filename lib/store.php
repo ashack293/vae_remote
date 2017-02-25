e7618b86e292d6e457fe92640aa1f1771a54f866
Kevin Bombino <kevin@bombino.org>
2017-02-25 12:37:00 -0800

    Remove extra bracket

--- lib/store.php
+++ lib/store.php
@@ -1744,7 +1744,7 @@ function _vae_store_render_previous_order($a, &$tag, $context, &$callback, $rend
   global $_VAE;
   _vae_session_deps_add('__v:store', '_vae_store_render_previous_order');
   $data = vae_store_previous_orders();
-  $pdata = $pdata[$_REQUEST['order']]];
+  $pdata = $pdata[$_REQUEST['order']];
   if (!$pdata) {
     return vae_redirect("/");
   }
